2e16c1371326e64b1f1a2965f6a1cf0125cd6b35
Itay Flexusaurs <eitayhaim@gmail.com>
2023-06-18 22:31:11 +0300

    scraping base impl

--- src/main.py
+++ src/main.py
@@ -1,19 +1,14 @@
-import bot
-import scrape_utils
-import env
+from flask import Flask, request
+import requests
+from bs4 import BeautifulSoup
+import boto3
+import telebot
+import os
 
+app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('product_prices_table')
 
-
-
-if __name__ ==  main:
-    main()
-    
-    
-def main():
-    
-
-
-
-
-main()
-
+@app.route('/' + os.getenv('TELEGRAM_TOKEN'), methods=['POST'])
+def receive_update():
+    data = request.json
--- src/scrape_utils.py
+++ src/scrape_utils.py
@@ -1,26 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
-import boto3
-import env 
+import json
+from dotenv import load_dotenv
+import os
 from datetime import datetime
 import time
-import telegram from telegram
 
-dynamodb = boto3.resource('')
-table = ()
+def scrape(link: str, currency = "ILS"):
+    headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+    page = requests.get(link, headers=headers)
+    soup = BeautifulSoup(page.text, "html.parser").find("script", type="application/ld+json")
+    product_data = json.loads(soup.string)
+    print(product_data["name"], product_data["color"], product_data["productID"])
+    #price_endpoint = f"https://www.asos.com/api/product/catalogue/v3/stockprice?productIds={product_data['productID']}&store=ROW&currency={currency}"
+    price_api = f"https://api.asos.com/product/catalogue/v3/stockprice?productIds={product_data['productID']}&store=ROW&currency={str(currency)}"
+    print(requests.get(price_api, headers=headers).json()[0]["productPrice"]["current"]["text"], requests.get(price_api, headers=headers).json()[0]["discountPercentage"])
+#link = "https://www.asos.com/tommy-jeans/tommy-jeans-chest-luxe-logo-t-shirt-in-black/prd/204291070?colourWayId=204291073&cid=50580"
 
-headers = {
-    'authority': 'www.asos.com',
-    'pragma': 'no-cache',
-    'cache-control': 'no-cache',
-    'user-agent': 'Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4159.2 Safari/537.36',
-    'accept': '*/*',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': link,
-    'accept-language': 'en-US,en;q=0.9'
-}
-
-
- 
+if __name__ == "__main__":
+    scrape("https://www.asos.com/reclaimed-vintage/reclaimed-vintage-swim-shorts-in-short-length-in-blue-paisely-print/prd/204266239?colourWayId=204266240&cid=13210", currency="ILS")
+    
